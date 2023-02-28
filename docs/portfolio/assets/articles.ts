6ec5b1e670252201f8eee0e48c738b12f1cf070b
ribble_T <rnarad@uw.edu>
2023-02-28 14:55:16 -0800

    maybe build for prod?

---
+++ docs/portfolio/assets/articles.ts
@@ -0,0 +1,18 @@
+export interface articles {
+    "projects": [
+        [
+            {"title" : "Honors 100"},
+            {"date" : "Feb 26 2022"},
+            {"category": "Paper"}],
+        [
+            {"title" : "Honors 200"},
+            {"date" : "Feb 27 2022"},
+            {"category": "News"}
+        ],
+    ],
+    "academics": [
+        {"title" : "Honors 100"},
+        {"date" : "Feb 26 2022"},
+        {"category": "Paper"}
+    ]
+}
